60c4cabd164cb8759fe24b5b90b22f291f2e83e2
Melany Estevez Camata <63152500+Mademoisellefou@users.noreply.github.com>
2022-05-13 11:51:03 -0500

    F#
    Calculando 2 variables

---
+++ Pregunta6/PPregunta6/Pregunta6/Program.fs
@@ -0,0 +1,28 @@
+﻿
+// Nombre: Estevez Camata Melany
+open System 
+[<EntryPoint>]
+let main argv = 
+    let suma a b = a + b
+    let resta a b = a - b
+    let multi a b = a * b
+    let division a b = a / b
+    Console.WriteLine("========================")
+    Console.WriteLine("Calculador Basico\nIngrese un numero")
+    let num1=Convert.ToDouble(Console.ReadLine())
+    Console.WriteLine("Ingrese un numero")
+    let num2=Convert.ToDouble(Console.ReadLine())
+    Console.WriteLine("Calculando ...")
+    Console.WriteLine("La Suma es :")
+    Console.WriteLine(suma num1 num2)
+    Console.WriteLine("La Resta es :")
+    Console.WriteLine(resta num1 num2)
+    Console.WriteLine("La Multiplicacion es :")
+    Console.WriteLine(multi num1 num2)
+    Console.WriteLine("La Division es :")
+    Console.WriteLine(division num1 num2)
+    Console.WriteLine("========================")
+    let tecla=Console.ReadKey()
+    0
+
+    
